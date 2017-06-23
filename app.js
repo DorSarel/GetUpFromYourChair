8c30d56ace9aea42a653d08fa0b0d71d4848444c
Dor Sarel <Dor.Sarel@sandisk.com>
2017-06-23 14:07:32 +0300

    Modified app.js

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.get("/", function(req, res){
 	res.render("index.ejs");
 });
 
-app.listen(3000, "localhost", function(){
-	console.log("Server is on....");
+var port = process.env.PORT || 3000;
+
+app.listen(port, function(){
+	console.log("Server is on...");
 });

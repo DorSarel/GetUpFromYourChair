4508260dd82bf889fadf3f08470ed32d2ba2e181
Dor Sarel <Dor.Sarel@sandisk.com>
2017-06-25 11:10:56 +0300

    Added countdown timer between notifications

--- public/scripts/fire.js
+++ public/scripts/fire.js
@@ -8,7 +8,8 @@ will notify using javaScript notification to the user that it is time to get up.
 //Button var
 const $btn = $("#fire");
 const $userTime = $("#user_time");
-const $timer = $("#timer");
+const $bContainer = $(".container-body");
+const $timer = $("<div id='timer'</div>");
 
 const timerForDelay = 1000 * (5 * 60);
 let timeForInterval = 1000 * (120 * 60); // 1000 miliseconds = 1 second * 120 minutes
@@ -19,6 +20,12 @@ if (Notification.permission !== "granted"){
 	Notification.requestPermission();
 }
 
+
+/*
+delay_5
+Responsible for creating second notification,
+5 minutes after the first one in order to remind the user to get up in case he didn't.
+*/
 function delay_5() {
 	var delay_notification = new Notification("Come on!", {
 		body: "Did you get up already?!?!?!",
@@ -30,6 +37,11 @@ function delay_5() {
 	}
 }
 
+/*
+notifyMe
+Responsible for creating the first notification.
+User input decide when the notification will happen, if not user - default it 2 hours.
+*/
 function notifyMe() {
 
 	if (!Notification) {
@@ -46,8 +58,7 @@ function notifyMe() {
 			icon: "/img/Icons8-Ios7-Sports-Walking.ico"
 		});
 
-		// console.log(Date.now());
-
+		startCountDown();
 		notification.onclick = () => {
 			notification.close();
 			// setTimeout(delay_5, 10 * 1000); //For debug
@@ -56,6 +67,42 @@ function notifyMe() {
 	}
 }
 
+/*
+startCountDown
+Responsilbe for setting countdown between notifications.
+on countdown finish - notifyMe will trigger.
+*/
+function startCountDown() {
+
+	removeElem();
+	// Create the time element and append it to the container
+	$bContainer.append($timer);
+
+	//Setting the timer for the next break
+	$timer.countdown(Date.now() + timeForInterval, (e) => {
+		$timer.html(e.strftime("%H:%M:%S"));
+	});
+
+	$timer.on("finish.countdown", notifyMe);
+}
+
+
+/*
+removeElem
+Responsilbe for removing the $timer element.
+*/
+function removeElem() {
+	// Remove the timer element from the container
+	$timer.remove();
+}
+
+
+
+/*
+The button handler.
+Responsible for handling the click event.
+User input will be handled when button is clicked.
+*/
 $btn.on("click", () => {
 	let btnText = $btn.text();
 	let userVal = $userTime.val();
@@ -66,23 +113,25 @@ $btn.on("click", () => {
 			// console.log(userVal);
 			userVal = Number(userVal);
 
-			if (userVal > 180) {
+			if (userVal > 180 || userVal < 1) {
 				userVal = 180;
 			}
 			// console.log(userVal);
 
 			// Uses the user input to calculate the time
 			timeForInterval = 1000 * (userVal * 60);
-
 			// Sets the user input to be empty
 			$userTime.val("");
 		}
 
 		$btn.text("Stop Notification");
-		// intervalId = setInterval(notifyMe, 30 * 1000); //For debug
-		intervalId = setInterval(notifyMe, timeForInterval);
+		startCountDown();
+
 	} else {
 		$btn.text("Trigger Your Next Break");
-		clearInterval(intervalId);
+		$timer.countdown("stop");
+		$timer.html("");
+
+		removeElem();
 	}
 });
